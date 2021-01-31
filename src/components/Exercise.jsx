765f4b2da183d2299ff920c760bf44e21973d90f
Dylan Hensel <dylan@hensel.com>
2021-01-30 16:04:33 -0800

    adds sudo code for exercise generation function

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -9,7 +9,22 @@ function Exercise() {
   let [reps, setReps] = useState(0);
   // workout description
   let [description, setDescription] = useState("");
+  //workout array
+  const [exerciseArray, setExerciseArray] = useState({});
+  // key = exercise # or _id value
   // next - functianality - page reload with new content
+
+  // onload run newExercise to generate starting array
+  // onload set state (useState)
+  // newExercise(e === onload) called three times
+  const newExercise = (e) => {
+    // on click would check length of array, shift 0th exercise off array and push new random on, return shifted exercise as new exercise
+    // after length > some numbber 3 5 w/e
+    // select random number between 0 and database.length
+    // if previous 3 items in array is same id, re auto generate number
+    // push exercise at that id to array (& remove element if necessary)
+  };
+
   return (
     <div>
       <h1 className="workout-name">{workout}</h1>
