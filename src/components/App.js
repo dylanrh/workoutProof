1168c202f0c63f361b2491ea8371b8f94fc48d18
Dylan Hensel <dylan@hensel.com>
2021-04-11 15:04:14 -0700

    cleans up App file

--- src/components/App.js
+++ src/components/App.js
@@ -5,22 +5,9 @@ import Exercise from "./Exercise";
 // import ReactDom from 'react-dom'
 
 function App() {
-  // workout name
-  const [workout, setWorkout] = useState("");
-  // workout img
-  const [image, setImage] = useState("");
-  // number of excercies
-  const [number, setNumber] = useState(0);
-  // workout description
-  const [description, setDescription] = useState("");
-  // next - functianality - page reload with new content
   return (
     <div className="App">
-      {/* <h1>{workout}</h1> */}
-      {/* <Picture />
-      <About /> */}
       <Exercise />
-      {/* <button id="next">NEXT</button> */}
     </div>
   );
 }
